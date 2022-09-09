a2ec8d8477eba7e5d576fbc5cf3aa04fea0a986d
Parham Abolghasemi <parhamab17@gmail.com>
2022-09-09 09:47:58 +0430

    styles, mui icons, bugs fixed, header component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+// styles
+import "./assets/styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
